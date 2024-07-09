f7a42a68da82fa1c9888b68476456b568a662486
nburchha <niklas@burchhardt.de>
2024-07-09 09:08:38 +0200

    did ex02, should be fine. added .log to gitignore

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -3,10 +3,124 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nburchha <nburchha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: niklasburchhardt <niklasburchhardt@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 20:19:23 by nburchha          #+#    #+#             */
-/*   Updated: 2024/07/07 20:19:24 by nburchha         ###   ########.fr       */
+/*   Updated: 2024/07/09 09:06:53 by niklasburch      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_accountIndex = _nbAccounts - 1;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::Account()
+{
+	_nbAccounts++;
+	_totalAmount = 0;
+	_accountIndex = _nbAccounts - 1;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::~Account()
+{
+	_displayTimestamp();
+	_nbAccounts--;
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
+
+int Account::getNbAccounts()
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount()
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits()
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals()
+{
+	return _totalNbWithdrawals;
+}
+
+void Account::_displayTimestamp()
+{
+	time_t now = time(0);
+	tm* localTime = localtime(&now);
+	std::cout << "[" << localTime->tm_year + 1900
+			  << std::setw(2) << std::setfill('0') << localTime->tm_mon + 1
+			  << std::setw(2) << std::setfill('0') << localTime->tm_mday << "_"
+			  << std::setw(2) << std::setfill('0') << localTime->tm_hour
+			  << std::setw(2) << std::setfill('0') << localTime->tm_min
+			  << std::setw(2) << std::setfill('0') << localTime->tm_sec << "] ";
+}
+
+void Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit << ";amount:" << _amount + deposit << ";nb_deposits:" << _nbDeposits + 1 << std::endl;
+	_amount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+	if (_amount - withdrawal <= 0)
+	{
+		std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:refused" << std::endl;
+		return false;
+	}
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:" << withdrawal << ";amount:" << _amount - withdrawal << ";nb_withdrawal:" << _nbWithdrawals + 1 << std::endl;
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	return true;
+}
+
+int Account::checkAmount() const
+{
+	return _amount;
+}
